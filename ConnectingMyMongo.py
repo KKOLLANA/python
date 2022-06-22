031aa7add77731da0de2557cf6aa876cd1e24cff
KKOLLANA <kiran-kumar.kollana@capgemini.com>
2022-06-22 11:13:51 +0530

    changes made

--- ConnectingMyMongo.py
+++ ConnectingMyMongo.py
@@ -1,13 +1,13 @@
 import pymongo  #import the package
 
-client=pymongo.mongoclient('mongodb://127.0.0.1:27017')
+client=pymongo.MongoClient('mongodb://127.0.0.1:27017')
 mydb=client['Employee'] #With this it will not create the databaste till we create the collections
 
-# information=mydb.employeeinformation #employeeinformation is a collection or table name here
-# record={
-#     'firstname':'kiran',
-#     'lastname':'kollana',
-#     'department':'MCA'
+information=mydb.employeeinformation #employeeinformation is a collection or table name here
+record={
+    'firstname':'kiran',
+    'lastname':'kollana',
+    'department':'MCA'
 
-# }
-# information.insert_one(record)
+}
+information.insert_one(record)
